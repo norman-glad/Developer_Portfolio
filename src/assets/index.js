e221f035d9a80d831c8f0dc8664dc263bf9665b9
Norman <normanglad26012004@gmail.com>
2025-01-19 18:05:40 -0500

    core editing done

---
+++ src/assets/index.js
@@ -0,0 +1,78 @@
+import bwmap from './backgrounds/bw-map.jpeg';
+import nairobi from './backgrounds/nairobi.png';
+import whiteabstract from './backgrounds/white-abstract.png';
+import worldmap from './backgrounds/world-map.png';
+
+import logo from './logo/logo-black.png';
+import logotext from './logo/logo-text-black.png';
+
+import norm from './personal/itsme.png';
+
+import backend from './icons/backend.png';
+import ux from './icons/ux.png';
+import frontend from './icons/frontend.png';
+import prototyping from './icons/prototyping.png';
+import github from './icons/github.png';
+import close from './icons/close.png';
+import download from './icons/download.png';
+import downloadHover from './icons/downloadHover.png';
+import pineapple from './icons/pineapple.png';
+import pineappleHover from './icons/pineappleHover.png';
+import menu from './icons/menu.png';
+import resume from './icons/resume.png';
+import send from './icons/send.png';
+import sendHover from './icons/sendHover.png';
+
+import css from './tech/css.png';
+import docker from './tech/docker.png';
+import figma from './tech/figma.png';
+import git from './tech/git.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import nodejs from './tech/nodejs.png';
+import reactjs from './tech/reactjs.png';
+import redux from './tech/redux.png';
+import tailwind from './tech/tailwind.png';
+import typescript from './tech/typescript.png';
+import graphql from './tech/graphql.png';
+import postgresql from './tech/postgresql.png';
+
+
+
+
+export {
+  bwmap,
+  nairobi,
+  whiteabstract,
+  worldmap,
+  logo,
+  logotext,
+  norm,
+  backend,
+  ux,
+  frontend,
+  prototyping,
+  github,
+  close,
+  download,
+  downloadHover,
+  pineapple,
+  pineappleHover,
+  menu,
+  resume,
+  send,
+  sendHover,
+  css,
+  docker,
+  figma,
+  git,
+  graphql,
+  html,
+  javascript,
+  nodejs,
+  postgresql,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,155 @@
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  html,
+  css,
+  reactjs,
+  tailwind,
+  nodejs,
+  git,
+  docker,
+  postgresql,
+  stock,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Frontend Developer',
+    icon: frontend,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  {
+    title: 'UI/UX Design',
+    icon: ux,
+  },
+  {
+    title: 'Software Prototyping',
+    icon: prototyping,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'postgresql',
+    icon: postgresql,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'docker',
+    icon: docker,
+  },
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'Full-Stack Bookstore Website',
+    description: 'A basic web-application featuring JWT-based authentication and secured routes for user login and book management',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'jwt',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: stock,
+    repo: 'https://github.com/norman-glad/please-work',
+    demo: 'https://please-work-1.onrender.com/',
+  },
+  {
+    id: 'project-2',
+    name: 'Java Game Tracker Application',
+    description:
+      'A java-swing based application for storing and managing game and player data',
+    tags: [
+      {
+        name: 'java',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'sql',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: stock,
+    repo: 'https://github.com/norman-glad/java-db-fx',
+  },
+  {
+    id: 'project-3',
+    name: 'Spam Comment Classifier',
+    description: 'Python scikit based program to classify comments as spam or ham',
+    tags: [
+      {
+        name: 'python',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'scikit',
+        color: 'green-text-gradient',
+      },
+    ],
+    image: stock,
+    repo: 'https://github.com/norman-glad',
+  },
+];
+
+export { services, technologies, projects };
